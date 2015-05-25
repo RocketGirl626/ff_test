a80f955c6c5fffaac190d6acbb22c847df2e2446
Mom <mom@Michaels-MacBook-Pro-2.local>
2015-05-25 13:31:36 -0700

    add initial files.

---
+++ db/migrate/20150525202326_add_initial_tables.rb
@@ -0,0 +1,35 @@
+class AddInitialTables < ActiveRecord::Migration
+
+  def change
+  	create_table :recipes do |t|
+  		t.string :recipe_name
+  		t.string :source
+  		t.string :servings
+  		t.string :comment
+  		t.timestamps null: false
+  	end
+
+    create_table :categories do |t|
+      t.string :cat_name
+      t.timestamps null: false
+    end
+
+    create_table :categories_recipes, id: false do |t|
+      t.belongs_to :recipe, index: true
+      t.belongs_to :category, index: true
+    end
+
+    create_table :ingredients do |t|
+    	t.string :ingredient_name
+    	t.integer :recipe_id
+    	t.timestamps null: false
+    end
+
+    create_table :instructions do |t|
+    	t.string :instruction_name
+    	t.integer :recipe_id
+    	t.timestamps null: false
+    end
+  end
+end
+  
